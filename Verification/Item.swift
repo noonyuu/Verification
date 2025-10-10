9aadffeb4408172344a83721f72131244da73225
noonyuu <noon3m.op.sh@gmail.com>
2025-10-11 02:09:04 +0900

    init

---
+++ Verification/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Verification
+//
+//  Created by shimizu on 2025/10/10.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
